4295636f8c4e647cc3e2c6a752abcf6383caa65a
aastha.jain <aastha.jain@joshtechnologygroup.com>
2021-08-03 15:42:46 +0530

    [AS2-1]: Completed the base setup of AS2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Assignment-2
 
+# Project setup
+To run the Assignment on your system. Perform the following steps:
+
+1. Install node on your system which comes with npm package manager.
+2. Clone this repository to your system.
+3. Open the terminal, go inside the clones directory and run 'npm install'
+4. The npm install you did in previous step should install all the dependencies, which you can confirm by visiting the node_modules in your project directory. 
+5. To build the changes run the command 'npm run build' . This will create a folder named 'dist' which will contained the bundle created by webpack.
+6. To go live and see the project running , run the command 'npm start'.
+7. ONce you run the above command your project will get live on localhost:8080.
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import 'styles';
+
+import 'images/architecture.png';
+import 'images/computer.png';
+import 'images/construction.png';
+import 'images/consultation-bg.png';
+import 'images/exporting.png';
+import 'images/hero-bg.png';
+
+import 'fonts/helvetica-neue.woff';
+import 'fonts/helvetica-neue.woff2';
+import 'fonts/helvetica-neueLTStd-Bd.woff';
+import 'fonts/helvetica-neueLTStd-Bd.woff2';
+import 'fonts/helvetica-neueLTStd-Blk.woff';
+import 'fonts/helvetica-neueLTStd-Blk.woff2';
+import 'fonts/helvetica-neueLTStd-Lt.woff';
+import 'fonts/helvetica-neueLTStd-Lt.woff2';
+import 'fonts/helvetica-neueLTStd-roman.woff';
+import 'fonts/helvetica-neueLTStd-roman.woff2';
+import 'fonts/icomoon.eot';
+import 'fonts/icomoon.svg';
+import 'fonts/icomoon.ttf';
+import 'fonts/icomoon.woff';

27251a671cccc641239d3c5062007a564ccb5597
aastha.jain <aastha.jain@joshtechnologygroup.com>
2021-08-12 19:20:02 +0530

    [AS2-2]: Created navbar for the homepage

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import 'styles';
+
+(() => {
+    const main = () => {
+        const navbarToggler = document.querySelector(".navbar__toggler");
+        const navbarIcon = document.querySelector(".navbar__icon");
+        const navbarPopUp  = document.querySelector(".navbar__items");
+        const navbarHeader = document.querySelector(".navbar__top");
+        const openNavbar = () => {
+            navbarIcon.classList.toggle("icon-menu");
+            navbarIcon.classList.toggle("icon-close");
+            navbarPopUp.classList.toggle("navbar__items--open");
+            navbarHeader.classList.toggle("navbar__top--open");
+        };
+        navbarToggler.addEventListener("click", openNavbar);
+    }
+    document.addEventListener('DOMContentLoaded', main);
+})();
